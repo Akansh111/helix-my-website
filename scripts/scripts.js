0594e8d4f402f85c9a4ca125c41c67e5d69e45f4
Akansh Goel <akanshgoel@akanshs-mbp.corp.adobe.com>
2023-12-11 11:54:04 +0530

    aem frankline capstone

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,51 @@
 import {
-  sampleRUM,
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForLCP,
-  loadBlocks,
-  loadCSS,
-} from './lib-franklin.js';
+	sampleRUM,
+	buildBlock,
+	loadHeader,
+	loadFooter,
+	decorateButtons,
+	decorateIcons,
+	decorateSections,
+	decorateBlocks,
+	decorateTemplateAndTheme,
+	waitForLCP,
+	loadBlocks,
+	loadCSS,
+	fetchPlaceholders,
+} from "./aem.js";
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
-window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
 
+/**
+ * Builds hero block and prepends to main in a new section.
+ * @param {Element} main The container element
+ */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
+	const h1 = main.querySelector("h1");
+	const picture = main.querySelector("picture");
+	// eslint-disable-next-line no-bitwise
+	if (
+		h1 &&
+		picture &&
+		h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING
+	) {
+		const section = document.createElement("div");
+		section.append(buildBlock("hero", { elems: [picture, h1] }));
+		main.prepend(section);
+	}
+}
+
+/**
+ * load fonts.css and set a session storage flag
+ */
+async function loadFonts() {
+	await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+	try {
+		if (!window.location.hostname.includes("localhost"))
+			sessionStorage.setItem("fonts-loaded", "true");
+	} catch (e) {
+		// do nothing
+	}
 }
 
 /**
@@ -32,12 +53,12 @@ function buildHeroBlock(main) {
  * @param {Element} main The container element
  */
 function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
+	try {
+		buildHeroBlock(main);
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.error("Auto Blocking failed", error);
+	}
 }
 
 /**
@@ -46,79 +67,78 @@ function buildAutoBlocks(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
+	// hopefully forward compatible button decoration
+	decorateButtons(main);
+	decorateIcons(main);
+	buildAutoBlocks(main);
+	decorateSections(main);
+	decorateBlocks(main);
 }
 
 /**
- * loads everything needed to get to LCP.
+ * Loads everything needed to get to LCP.
+ * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    await waitForLCP(LCP_BLOCKS);
-  }
+	document.documentElement.lang = "en";
+	decorateTemplateAndTheme();
+	const main = doc.querySelector("main");
+	if (main) {
+		decorateMain(main);
+		document.body.classList.add("appear");
+		await waitForLCP(LCP_BLOCKS);
+	}
+
+	try {
+		/* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+		if (
+			window.innerWidth >= 900 ||
+			sessionStorage.getItem("fonts-loaded")
+		) {
+			loadFonts();
+		}
+	} catch (e) {
+		// do nothing
+	}
 }
 
 /**
- * Adds the favicon.
- * @param {string} href The favicon URL
- */
-export function addFavIcon(href) {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.type = 'image/svg+xml';
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
-}
-
-/**
- * loads everything that doesn't need to be delayed.
+ * Loads everything that doesn't need to be delayed.
+ * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadBlocks(main);
+	const main = doc.querySelector("main");
+	await loadBlocks(main);
 
-  const { hash } = window.location;
-  const element = hash ? doc.getElementById(hash.substring(1)) : false;
-  if (hash && element) element.scrollIntoView();
+	const { hash } = window.location;
+	const element = hash ? doc.getElementById(hash.substring(1)) : false;
+	if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+	loadHeader(doc.querySelector("header"));
+	loadFooter(doc.querySelector("footer"));
 
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
-  sampleRUM('lazy');
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  sampleRUM.observe(main.querySelectorAll('picture > img'));
+	loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+	loadFonts();
+
+	sampleRUM("lazy");
+	sampleRUM.observe(main.querySelectorAll("div[data-block-name]"));
+	sampleRUM.observe(main.querySelectorAll("picture > img"));
 }
 
 /**
- * loads everything that happens a lot later, without impacting
- * the user experience.
+ * Loads everything that happens a lot later,
+ * without impacting the user experience.
  */
 function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
+	// eslint-disable-next-line import/no-cycle
+	window.setTimeout(() => import("./delayed.js"), 3000);
+	// load anything that can be postponed to the latest here
 }
 
 async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
+	await loadEager(document);
+	await loadLazy(document);
+	loadDelayed();
 }
 
 loadPage();
